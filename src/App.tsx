060e8248b4b0b7edf115b6fd787f1342f1579bdf
miniusaco <80330292+miniusaco@users.noreply.github.com>
2022-05-15 14:39:24 +0900

    55.propsに型定義

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ type TodoType = {
   userId: number;
   id: number;
   title: string;
-  completed: boolean;
+  completed?: boolean;
 };
 
 export default function App() {
@@ -24,7 +24,7 @@ export default function App() {
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} userid={todo.userId} />
+        <Todo title={todo.title} userId={todo.userId} />
       ))}
     </div>
   );
