fde398bece8fcb6f3742f49ff0ec729d35262b29
miniusaco <80330292+miniusaco@users.noreply.github.com>
2022-05-18 21:50:40 +0900

    58.オプショナルチェインニングnull

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import "./styles.css";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+
+const user: User = {
+  name: "いぬっこ",
+  hobbies: ["映画", "ゲーム"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -17,6 +23,7 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
